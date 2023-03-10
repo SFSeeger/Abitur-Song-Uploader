33c627aa7989b6606a02ebbd43e542188de6826f
Simon Felix Seeger <simon.f.seeger@gmx.de>
2023-03-10 22:16:38 +0100

    Ansible setup

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-set -u
-
-git pull origin main
-
-pip install -r /home/gunicorn/Abitur-Song-Uploader/requirements.txt
-npm install --prefix /home/gunicorn/Abitur-Song-Uploader/src/theme
