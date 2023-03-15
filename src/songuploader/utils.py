a659fcd9dc1bea59bc22c25f3d7e2957eb75a88a
julySteuer <xghostx2709@gmail.com>
2023-03-15 17:25:40 +0100

    feature/10: Fixed error sowwy >~<

--- src/songuploader/utils.py
+++ src/songuploader/utils.py
@@ -15,9 +15,9 @@ class LoginRequiredTemplateView(LoginRequiredMixin, TemplateView):
     template_name: str = None
 
 def download_song(url: str, submission: Submission):
-    out_path = os.path.join("/tmp", f"{submission.user.username}_temp.wav");
-    YouTube(url=url).streams.first().download(output_path=os.path.dirname(out_path), filename=os.path.basename(out_path))
-    open_file = open(out_path, 'r')
+    out_path = os.path.join("/tmp", f"{submission.user.username}_temp.wav")
+    YouTube(url=url).streams.filter(only_audio=True).first().download(output_path=os.path.dirname(out_path), filename=os.path.basename(out_path))
+    open_file = open(out_path, 'rb')
     song_file: File = File(open_file)
     submission.song = song_file
     submission.save()
