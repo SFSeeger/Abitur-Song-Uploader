22c6f5c5e9acbcf2cc78be2cc01441ac2781e7d7
SFSeeger <simon.f.seeger@gmx.de>
2023-04-17 10:38:22 +0200

    Download fix

--- src/songuploader/utils.py
+++ src/songuploader/utils.py
@@ -3,6 +3,7 @@ import shutil
 from datetime import datetime
 from typing import Optional
 
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.files import File
@@ -13,10 +14,6 @@ from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import TemplateView, View
 from pytube import YouTube
-from songuploader.settings import MEDIA_ROOT
-from django.core.files import File
-from uploader.models import Submission
-import os 
 
 from songuploader.settings import MEDIA_ROOT
 from uploader.models import Submission
@@ -29,7 +26,7 @@ class ConfiguredLoginViewMixin(LoginRequiredMixin):
 
 class LoginRequiredTemplateView(ConfiguredLoginViewMixin, TemplateView):
     template_name: str = None
-    
+
 
 class UnderConstructionView(View):
     def get(self, request, *args, **kwargs):
@@ -55,14 +52,15 @@ class DisabledOnDateMixin:
             return redirect(self.date_redirect_url)
         return super().dispatch(request, *args, **kwargs)
 
+
 class LoginRequiredTemplateView(LoginRequiredMixin, TemplateView):
     login_url = reverse_lazy("login")
     redirect_field_name = "redirect_to"
 
 
 def download_song(submission: Submission):
-    mp4_out_path = os.path.join(MEDIA_ROOT, "tmp", f"{submission.user.id}.mp4")
-    mp3_out_path = os.path.join(MEDIA_ROOT, "tmp", f"{submission.user.id}.mp3")
+    mp4_out_path = os.path.join(settings.MEDIA_ROOT, "tmp", f"{submission.user.id}.mp4")
+    mp3_out_path = os.path.join(settings.MEDIA_ROOT, "tmp", f"{submission.user.id}.mp3")
     YouTube(url=submission.song_url).streams.filter(only_audio=True).first().download(
         output_path=os.path.dirname(mp4_out_path),
         filename=os.path.basename(mp4_out_path),
@@ -77,10 +75,11 @@ def download_song(submission: Submission):
 
 
 def slice_song(submission: Submission):
-    out_path = os.path.join(MEDIA_ROOT,"tmp", os.path.basename(submission.song.name))
+    out_path = os.path.join(
+        settings.MEDIA_ROOT, "tmp", os.path.basename(submission.song.name)
+    )
     shutil.copy2(submission.song.path, out_path)
     os.system(
         f"ffmpeg -ss {submission.start_time} -i {out_path} -c copy -y -t {submission.end_time-submission.start_time} {submission.song.path}"
     )
     os.remove(out_path)
-
--- src/uploader/management/commands/download_songs.py
+++ src/uploader/management/commands/download_songs.py
@@ -1,16 +1,15 @@
 from django.core.management.base import BaseCommand
+from django_q.tasks import async_chain, async_task
+
+from songuploader.utils import download_song, slice_song
 from uploader.models import Submission
 from uploader.views.submission_views import download_slice
-from django_q.tasks import async_task
-from songuploader.utils import slice_song, download_song
-from django_q.tasks import async_chain
+
 
 class Command(BaseCommand):
     help = "To manually redownload/slice songs for users"
 
     def handle(self, *args, **options):
-        for submission in Submission.objects.filter(song=''):
+        for submission in Submission.objects.filter(song=None):
             if submission.song_url:
                 async_chain([(download_song, [submission]), (slice_song, [submission])])
-            else:
-                async_task(slice_song, submission)
