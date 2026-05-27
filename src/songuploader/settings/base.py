83ffbc7cfcf0c1ca7672019bf46cc7cb38221e51
Simon Felix Seeger <50206768+SFSeeger@users.noreply.github.com>
2026-05-27 21:36:27 +0200

    Add gzip middleware

--- src/songuploader/settings/base.py
+++ src/songuploader/settings/base.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     "django_prometheus.middleware.PrometheusBeforeMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.gzip.GZipMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
