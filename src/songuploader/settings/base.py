15e85387fbe33287ddc586f521502049cc90a42c
Simon Felix Seeger <50206768+SFSeeger@users.noreply.github.com>
2026-05-30 20:54:55 +0200

    Add ARM docker image

--- src/songuploader/settings/base.py
+++ src/songuploader/settings/base.py
@@ -37,9 +37,9 @@ if BASE_URL is not None:
     ALLOWED_HOSTS.append(BASE_URL)
 ALLOWED_HOSTS = [h.strip() for h in ALLOWED_HOSTS if h] if ALLOWED_HOSTS else []
 
-CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", None)
-if CSRF_TRUSTED_ORIGINS is not None:
-    CSRF_TRUSTED_ORIGINS = [h.strip() for h in CSRF_TRUSTED_ORIGINS.split(",") if h] if CSRF_TRUSTED_ORIGINS else []
+csrf_origins_env = os.environ.get("CSRF_TRUSTED_ORIGINS", None)
+if csrf_origins_env is not None:
+    CSRF_TRUSTED_ORIGINS = [h.strip() for h in csrv_origins_env.split(",") if h] if csrf_origins_env else []
 
 # Application definition
 
